3e7539d677b62a3b6483ce121e8cfabb4694808f
SmallAd <vadim.piontkovskiy@gmail.com>
2020-06-14 21:24:57 +0300

    Customer controller updated. Get customer by id added.

--- src/main/java/testgroup/springdemo/controller/CustomerController.java
+++ src/main/java/testgroup/springdemo/controller/CustomerController.java
@@ -3,6 +3,7 @@ package testgroup.springdemo.controller;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import testgroup.springdemo.model.Customer;
@@ -26,6 +27,11 @@ public class CustomerController {
     @GetMapping("/customers")
     public List<Customer> getCustomers() {
         return customerService.getCustomers();
+    }
+    
+    @GetMapping("/customers/{customerId}")
+    public Customer getCustomer(@PathVariable int customerId){
+        return customerService.getCustomer(customerId);
     }
 
 }
