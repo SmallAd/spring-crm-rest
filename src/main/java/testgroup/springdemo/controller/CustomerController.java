655f979ec9771d077b8de464601e46eb90a5d82d
SmallAd <vadim.piontkovskiy@gmail.com>
2020-06-14 22:50:47 +0300

    Customer controller updated. Update and Delete customers added.

--- src/main/java/testgroup/springdemo/controller/CustomerController.java
+++ src/main/java/testgroup/springdemo/controller/CustomerController.java
@@ -2,8 +2,12 @@ package testgroup.springdemo.controller;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import testgroup.springdemo.model.Customer;
@@ -15,7 +19,7 @@ import testgroup.springdemo.service.CustomerService;
  * @author smallad
  */
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/customers")
 public class CustomerController {
 
     private CustomerService customerService;
@@ -25,20 +29,40 @@ public class CustomerController {
         this.customerService = customerService;
     }
 
-    @GetMapping("/customers")
+    @GetMapping()
     public List<Customer> getCustomers() {
         return customerService.getCustomers();
     }
-    
-    @GetMapping("/customers/{customerId}")
-    public Customer getCustomer(@PathVariable int customerId){
+
+    @GetMapping("/{customerId}")
+    public Customer getCustomer(@PathVariable int customerId) {
         Customer customer = customerService.getCustomer(customerId);
-        
-        if(customer == null){
+
+        if (customer == null)
             throw new CustomerNotFoundException("Customer id not found - " + customerId);
-        }
-        
+
         return customer;
     }
 
+    @PostMapping()
+    public Customer addCustomer(@RequestBody Customer customer) {
+        customer.setId(0);
+        customerService.saveCustomer(customer);
+        return customer;
+    }
+    
+    @PutMapping()
+    public Customer updateCustomer(@RequestBody Customer customer){
+        customerService.saveCustomer(customer);
+        return customer;
+    }
+    
+    @DeleteMapping("/{customerId}")
+    public void deleteCustomer(@PathVariable int customerId){
+        Customer customer = customerService.getCustomer(customerId);
+        if(customer == null)
+            throw new CustomerNotFoundException("Customer id not found - " + customerId);
+        
+        customerService.deleteCustomer(customerId);
+    }
 }
